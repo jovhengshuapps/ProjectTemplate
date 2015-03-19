e42adf851af0bf99f2b3f80cba2b7195fdebb667
Joshua Jose Pecson <joshuajp@pressreader.com>
2015-03-20 05:45:56 +0800

    apiconnection class

---
+++ Autohaus/AppDelegate/Classes/api/APIAccessPoint.h
@@ -0,0 +1,95 @@
+//
+//  APIAccessPoint.h
+//  Template v.1 Variant 1
+//
+//  Make this class global by #import it to your prefix header file $[yourAppName]-Prefix.pch
+//
+//  Created by Angelo Lesano on 20/2/14.
+//  Copyright (c) 2014 Juz Media Pte Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface APIAccessPoint : NSObject <UIAlertViewDelegate>
+@property (strong, nonatomic)   NSString        *url;
+@property (strong, nonatomic)   NSDictionary    *postParam;
+@property (strong, nonatomic)   UIImage         *postImage;
+@property (strong, nonatomic)   NSString        *httpMethod;
+@property (strong, nonatomic)   NSString        *loadingMessage;
+@property (nonatomic)           BOOL            willShowCustomLoadingIndicator;
+@property (nonatomic)           BOOL            isLoading;
+
+- (void)connectWithCompletion:(void (^)(NSArray *response)) completion;
+- (void)alert:(NSString*)title message:(NSString*)message;
+
+// ----- APP SPECIFIC METHODS (like login, logout, save etc etc)
+
+
+
+@end
+// ----- METHOD TYPES ---- //
+#define kAPI_METHOD_POST        @"POST"
+#define kAPI_METHOD_GET         @"GET"
+
+// ----- LOAD URL HERE ---- //
+//URL
+#define kURL_Base               @"http://thanqueue-tqtq.rhcloud.com"
+
+//GET
+
+//POST
+#define kURL_POST_BaseForgotten             [kURL_Base stringByAppendingString:@"/ThanqueueApi/forgotPassword"]
+#define kURL_POST_BaseLogin                 [kURL_Base stringByAppendingString:@"/ThanqueueApi/login"]
+#define kURL_POST_BaseCreateAcct            [kURL_Base stringByAppendingString:@"/ThanqueueApi/signup"]
+#define kURL_POST_BaseVerification          [kURL_Base stringByAppendingString:@"/ThanqueueApi/verify"]
+
+#define kURL_POST_UserProfile               [kURL_Base stringByAppendingString:@"/ThanqueueApi/view"]
+#define kURL_POST_UnknownProfile            [kURL_Base stringByAppendingString:@"/ThanqueueApi/profile"]
+#define kURL_POST_UnknownProfileFollow      [kURL_Base stringByAppendingString:@"/ThanqueueApi/follow/add"]
+#define kURL_POST_UnknownProfileUnfollow    [kURL_Base stringByAppendingString:@"/ThanqueueApi/follow/delete"]
+
+#define kURL_POST_UserActivity              [kURL_Base stringByAppendingString:@"/ThanqueueApi/pff"]
+#define kURL_POST_UserPosts                 [kURL_Base stringByAppendingString:@"/ThanqueueApi/profilePost"]
+#define kURL_POST_UserEditProfile           [kURL_Base stringByAppendingString:@"/ThanqueueApi/editProfile"]
+
+#define kURL_POST_PostDelete                [kURL_Base stringByAppendingString:@"/ThanqueueApi/post/deletePost"]
+#define kURL_POST_PostLike                  [kURL_Base stringByAppendingString:@"/ThanqueueApi/post/like"];
+#define kURL_POST_PostUnlike                [kURL_Base stringByAppendingString:@"/ThanqueueApi/post/unlike"];
+#define kURL_POST_PostGetByID               [kURL_Base stringByAppendingString:@"/ThanqueueApi/getPost"]
+
+#define kURL_POST_TimelinePublic            [kURL_Base stringByAppendingString:@"/ThanqueueApi/publicPost"]
+#define kURL_POST_TimelinePrivate           [kURL_Base stringByAppendingString:@"/ThanqueueApi/privatePost"]
+#define kURL_POST_TimelineFollowing         [kURL_Base stringByAppendingString:@"/ThanqueueApi/followingPost"]
+#define kURL_POST_TimelineFollower          [kURL_Base stringByAppendingString:@"/ThanqueueApi/followerPostView"]
+
+#define kURL_POST_Explore                   [kURL_Base stringByAppendingString:@"/ThanqueueApi/explore"]
+#define kURL_POST_ListFollowing             [kURL_Base stringByAppendingString:@"/ThanqueueApi/follow/Following"]
+#define kURL_POST_ListFollowers             [kURL_Base stringByAppendingString:@"/ThanqueueApi/follow/Followers"]
+#define kURL_POST_ListAllContacts           [kURL_Base stringByAppendingString:@"/ThanqueueApi/users"]
+
+#define kURL_POST_SharePost                 [kURL_Base stringByAppendingString:@"/ThanqueueApi/post/add1"]
+#define kURL_POST_SharePostWithImage        [kURL_Base stringByAppendingString:@"/ThanqueueApi/post/add"]
+
+#define kURL_POST_Notification              [kURL_Base stringByAppendingString:@"/ThanqueueApi/notification"]
+#define kURL_POST_NotificationSeen          [kURL_Base stringByAppendingString:@"/ThanqueueApi/seen"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Autohaus/AppDelegate/Classes/api/WebserviceCall.m
+++ Autohaus/AppDelegate/Classes/api/WebserviceCall.m
@@ -7,20 +7,33 @@
 //
 
 #import "WebserviceCall.h"
+#import "APIAccessPoint.h"
 
 @implementation WebserviceCall
 
 -(void)initCallWithServiceURL:(NSString*)url withParameters:(NSDictionary *)parameters withCompletionHandler:(void(^)(id responseObject))completion{
     
-    NSURL *urlURL = [NSURL URLWithString:url];
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[urlURL standardizedURL]];
-    request.HTTPMethod = @"POST";
-    NSData *postData = [@"user_login=blah&user_pass=blah" dataUsingEncoding:NSUTF8StringEncoding allowLossyConversion:YES];
-    NSString *postLength = [NSString stringWithFormat:@"%lu",(unsigned long)[postData length]];
-    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
-    [request setValue:@"application/x-www-form-urlencoded; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
-    [request setValue:[@"user" dataUsingEncoding:NSUTF8StringEncoding] forKey:@"user_login"];
-    [request setValue:[@"pass" dataUsingEncoding:NSUTF8StringEncoding] forKey:@"user_pass"];
+    APIAccessPoint *accesspoint = [[APIAccessPoint alloc] init];
+    accesspoint.willShowCustomLoadingIndicator = YES;
+    accesspoint.url = WS_LOGIN;
+    accesspoint.httpMethod = @"POST";
+//    accesspoint.postImage = [UIImage imageNamed:@"image.png"];
+    accesspoint.postParam = parameters;
+    
+    [accesspoint connectWithCompletion:^(NSArray *response) {
+        NSLog(@"response:%@", response);
+    }];
+    
+    
+//    NSURL *urlURL = [NSURL URLWithString:url];
+//    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[urlURL standardizedURL]];
+//    request.HTTPMethod = @"POST";
+//    NSData *postData = [@"user_login=blah&user_pass=blah" dataUsingEncoding:NSUTF8StringEncoding allowLossyConversion:YES];
+//    NSString *postLength = [NSString stringWithFormat:@"%lu",(unsigned long)[postData length]];
+//    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+//    [request setValue:@"application/x-www-form-urlencoded; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
+//    [request setValue:[@"user" dataUsingEncoding:NSUTF8StringEncoding] forKey:@"user_login"];
+//    [request setValue:[@"pass" dataUsingEncoding:NSUTF8StringEncoding] forKey:@"user_pass"];
 //    [request setHTTPBody:postData];
     
     AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
@@ -39,21 +52,6 @@
 //    }] start];
 
     
-    [manager POST:WS_LOGIN parameters:nil constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
-        [formData appendPartWithFormData:[@"user" dataUsingEncoding:NSUTF8StringEncoding] name:@"user_login"];
-        [formData appendPartWithFormData:[@"pass" dataUsingEncoding:NSUTF8StringEncoding] name:@"user_pass"];
-    } success:^(AFHTTPRequestOperation *operation, id responseObject) {
-        completion(responseObject);
-    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-        NSLog(@"Response = %@", operation);
-        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error Retrieving Data"
-                                                            message:[error localizedDescription]
-                                                           delegate:nil
-                                                  cancelButtonTitle:@"Ok"
-                                                  otherButtonTitles:nil];
-        [alertView show];
-
-    }];
     
     
 //    [[manager HTTPRequestOperationWithRequest:request success:^(AFHTTPRequestOperation *operation, id responseObject) {
