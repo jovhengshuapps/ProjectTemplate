ed23a8a61aefd82c934b53e6156906bdc7e20091
Joshua Jose Pecson <joshuajp@pressreader.com>
2015-03-26 13:59:20 +0800

    updates

--- Autohaus/Resources/Targets/CST_Trading/app_headers/WebserviceCallURL.h
+++ Autohaus/Resources/Targets/CST_Trading/app_headers/WebserviceCallURL.h
@@ -9,19 +9,21 @@
 #ifndef Autohaus_WebserviceCallURL_h
 #define Autohaus_WebserviceCallURL_h
 
-#define WS_LOGIN                         @"http://autohaus.com.sg/services/?type=login"
-#define WS_LOGOUT                        @"http://autohaus.com.sg/services/?type=logout"
+#define WS_LOGIN                         @"http://www.csttrade.sg/services/?type=login"
+#define WS_LOGOUT                        @"http://www.csttrade.sg/services/?type=logout"
 #define WS_INQUIRY                       @"http://autohaus.com.sg/services/?type=inquiry"
 #define WS_UPDATE_INFO                   @"http://autohaus.com.sg/services/?type=update"
 #define WS_CHECKOUT                      @"http://autohaus.com.sg/services/?type=checkout"
-#define WS_GET_CART                      @"http://autohaus.com.sg/services/?type=getcart"
-#define WS_ADD_TO_CART                   @"http://autohaus.com.sg/services/?type=add_cart"
+#define WS_GET_CART                      @"http://www.csttrade.sg/services/?type=getcart"
+#define WS_ADD_TO_CART                   @"http://www.csttrade.sg/services/?type=add_cart"
 #define WS_COUNTRY                       @"http://autohaus.com.sg/services/?type=Country"
 #define WS_CATEGORY                      @"http://autohaus.com.sg/services/?type=category"
 #define WS_CM_SUBCATEGORY                @"http://autohaus.com.sg/services/?type=sub_category"
-#define WS_REGISTER                      @"http://autohaus.com.sg/services?type=register"
-#define WS_PRODUCTS                      @"http://autohaus.com.sg/services/?type=products"
-#define WS_RESET_PASSWORD                @"http://autohaus.com.sg/services/?type=reset"
-#define WS_CHANGE_PASSWORD               @"http://autohaus.com.sg/services/?type=change_password"
+#define WS_REGISTER                      @"http://www.csttrade.sg/services?type=register"
+#define WS_PRODUCTS                      @"http://www.csttrade.sg/services/?type=products"
+#define WS_RESET_PASSWORD                @"http://www.csttrade.sg/services/?type=reset"
+#define WS_CHANGE_PASSWORD               @"http://www.csttrade.sg/services/?type=change_password"
+#define WS_BANNERS                       @"http://www.csttrade.sg/services?type=banners"
+#define WS_TESTIMONIALS                  @"http://www.csttrade.sg/services/?type=testimonials"
 
 #endif
